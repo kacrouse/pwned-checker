74979dffbb9e3e9242ec7eecc794200a3714ff29
Kyle Crouse <crouse.kyle@gmail.com>
2020-04-19 16:51:56 -0600

    extract search input component

---
+++ client/src/SearchInput.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import {
+  Button,
+  ControlGroup,
+  InputGroup,
+  HTMLSelect,
+} from "@blueprintjs/core";
+
+export default function SearchInput({handleSearch}) {
+  const [searchType, setSearchType] = useState("account");
+  const [searchValue, setSearchValue] = useState("");
+
+  return (
+    <ControlGroup>
+      <HTMLSelect
+        value={searchType}
+        onChange={(evt) => setSearchType(evt.target.value)}
+        large={true}
+      >
+        <option value="account">Email</option>
+        <option value="password">Password</option>
+      </HTMLSelect>
+      <InputGroup
+        type={searchType === "password" ? "password" : "email"}
+        value={searchValue}
+        onChange={(evt) => setSearchValue(evt.target.value)}
+        large={true}
+        fill={true}
+      />
+      <Button icon="arrow-right" onClick={() => handleSearch({searchType, searchValue})} />
+    </ControlGroup>
+  );
+}
